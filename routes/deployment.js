d1d3adcb5ec8d44ab1842599f2958351aa12983c
be116-live <be116.live@gmail.com>
2019-03-09 07:23:46 +0900

    fixed

--- routes/deployment.js
+++ routes/deployment.js
@@ -4,10 +4,11 @@ const exec    = require('child_process').exec;
 
 router.post('/github', function(req, res, next) {
   let event = req.get('X-GitHub-Event');
+  // TODO SECRETを比較
   if (event == 'push') {
     console.log('Githubからデプロイしました。');
-    exec('git pull origin master');
-    exec('npm install');
+    exec('git -C $HOME/production pull origin master');
+    exec('npm -prefix $HOME/production install $HOME/production');
     res.send("GitHubからデプロイしました。");
   } else {
     console.log('不正なリクエストです。');
